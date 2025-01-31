6d928ce377ac19f365bc31648957a8eac702c823
CVMFKR <cam.reyesmora@gmail.com>
2025-01-30 22:54:53 -0300

    Arreglo con IA

--- src/data/benefitsData.js
+++ src/data/benefitsData.js
@@ -5,22 +5,22 @@ module.exports = {
     },
     2: {
         title: "Beneficios gratuitos CONSALUD 🏥",
-        content: `- Exámenes costo cero (Rayos - Ecografías - Laboratorio)\n- Telemedicina Gratuita\n- Descuentos en Farmacias\n- Buscador Online Mejores Convenios\n- Salcobrand desde un 12% hasta un 70%\n- 40% de cobertura dental en Red Salud\n- Descuentos en Ópticas Rotter y Kraus y GMO\nPDF informativo: https://1drv.ms/b/s!AjucDJ3soG62hJdvkm2kF46fj4XMaw?e=p97Od0`
+        content: `- Exámenes costo cero\n- Telemedicina Gratuita\n- Descuentos en Farmacias\nPDF: https://1drv.ms/b/s!AjucDJ3soG62hJdvkm2kF46fj4XMaw?e=p97Od0`
     },
     3: {
         title: "Beneficios gratuitos ESENCIAL 🏥",
-        content: `- Cobertura en Red de Prestadores Médicos\n- Remedios sin costo adicional (solo para personas con discapacidad)\nPDF informativo: https://1drv.ms/b/s!AjucDJ3soG62hKduxXduw7P_Mfwtpg?e=FpdPYB`
+        content: `- Cobertura en Red de Prestadores\n- Remedios sin costo para discapacitados\nPDF: https://1drv.ms/b/s!AjucDJ3soG62hKduxXduw7P_Mfwtpg?e=FpdPYB`
     },
     4: {
-        title: "Beneficios gratuitos NUEVA MAS VIDA 🏥",
-        content: `- Cobertura Plus en todos los planes\n- Beneficios acceso a Terapias, psicólogos y rehabilitación\nPDF informativo: https://1drv.ms/b/s!AjucDJ3soG62hJdqG57eE3e6bVEZTg?e=k7SkjY`
+        title: "Beneficios NUEVA MAS VIDA 🏥",
+        content: `- Terapias y psicólogos\n- Rehabilitación\nPDF: https://1drv.ms/b/s!AjucDJ3soG62hJdqG57eE3e6bVEZTg?e=k7SkjY`
     },
     5: {
-        title: "Beneficios gratuitos COLMENA 🏥",
-        content: `- Cobertura en Clínica Alemana\n- Médico a domicilio\n- Descuentos por Farmacia Ahumada y Cruz Verde\n- Consultas médicas en línea sin costo adicional\nPDF informativo: https://1drv.ms/b/s!AjucDJ3soG62hKfAGwGEp1XYhD57lQ?e=2vfNjO`
+        title: "Beneficios COLMENA 🏥",
+        content: `- Clínica Alemana\n- Médico a domicilio\nPDF: https://1drv.ms/b/s!AjucDJ3soG62hKfAGwGEp1XYhD57lQ?e=2vfNjO`
     },
     6: {
-        title: "Beneficios gratuitos VIDA TRES 🏥",
-        content: `- Consultas médicas, psicólogos y nutricionistas sin costo adicional\n- Acceso a emergencias médicas\nPDF informativo: https://1drv.ms/b/s!AjucDJ3soG62hLfhhqTSL72VuDP3qg?e=Em0lDj`
+        title: "Beneficios VIDA TRES 🏥",
+        content: `- Consultas médicas sin costo\n- Emergencias\nPDF: https://1drv.ms/b/s!AjucDJ3soG62hLfhhqTSL72VuDP3qg?e=Em0lDj`
     }
 };
